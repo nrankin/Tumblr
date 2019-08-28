4b4189f1fb6de560304c1fb78fbba700bb85ec74
Nicola Rankin <nicola.rankin@gmail.com>
2019-08-28 14:21:50 +1200

    added posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_030400) do
+ActiveRecord::Schema.define(version: 2019_08_28_021613) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
